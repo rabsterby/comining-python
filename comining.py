f649aeb2327a1d69beba4a2601a2f4673840e601
rabster <rabster.by@gmail.com>
2019-10-13 14:53:10 +0300

    Update comining.py

--- comining.py
+++ comining.py
@@ -1,4 +1,18 @@
-import flask
+# import flask
+KEY = X5szT5DjaoXhkuVmGVnMrBU
+URL = https://api.comining.io/?key= + KEY
+
+import datetime
+import urllib.request
+from urllib.parse import urljoin 
+import time
+import gzip
+import json
+import html
+import re
+import operator
+import os
+import binascii
 
 # https://api.comining.io/?key=[YOUR_API_KEY]
 # {"method":"[METHOD_NAME]"}
@@ -9,3 +23,15 @@ def worker(filmID):
   
 {"method":"coins_list"}
 
+
+r = requests.get('https://api.github.com/user', auth=('user', 'pass'))
+>>> r.status_code
+200
+>>> r.headers['content-type']
+'application/json; charset=utf8'
+>>> r.encoding
+'utf-8'
+>>> r.text
+u'{"type":"User"...'
+>>> r.json()
+{u'private_gists': 419, u'total_private_repos': 77, ...}
