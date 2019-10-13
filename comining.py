e314b6b135afd34cb5069ba7b3b312afd044b5df
rabster <rabster.by@gmail.com>
2019-10-13 13:56:34 +0300

    Update comining.py

--- comining.py
+++ comining.py
@@ -1,3 +1,11 @@
 import flask
 
-def 
+# https://api.comining.io/?key=[YOUR_API_KEY]
+# {"method":"[METHOD_NAME]"}
+
+def worker(filmID):
+    {"method":"workers_summary"}
+    return result
+  
+{"method":"coins_list"}
+
