c026df33c2200584d885c52f60b7c288797ed631
rabster <rabster.by@gmail.com>
2019-11-04 22:12:35 +0300

    Update comining-blocksdb.py

--- comining-blocksdb.py
+++ comining-blocksdb.py
@@ -33,14 +33,18 @@ for i in range(len(blcklst)):
 	#pref.update({cnn : 0})
 	#cn = pref.find({cnn :})
 	
+	dictccn = pref.find_one({'last': 'block'})
+	ccn = dictccn.get('count')
+	cn = dictccn.get('lastbl')
+
 	if  mnblocks.find({"id": ids}).count() == 0:
 		print(blcklst[i].get('coin'), blkNmbr, 'new', blcklst[i].get('status'))
 		if blcklst[i].get('reward') != None:
 			blk = {'id': blcklst[i].get('id'), 'coin': blcklst[i].get('coin'), 'blockNumber': blcklst[i].get('blockNumber'), 'reward': (int(int(blcklst[i].get('reward')) * 0.99)),'created': blcklst[i].get('created')}
 			mnblocks.insert(blk)
+			if blcklst[i].get('coin') == cn:
+				ccn = ccn + 1
+			else:
+				ccn = 1
 			pref.update({'last': 'block'},{'last': 'block', 'lastbl': blcklst[i].get('coin'), 'count': 1, 'date': blcklst[i].get('created')})
 			print(blcklst[i].get('coin'), blkNmbr, 'inserted')
-			
-			
-
-			
